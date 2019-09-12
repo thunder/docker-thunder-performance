a754a0b8f3001db9aece94bb7d689da7df77ffa1
Mladen Todorovic <mladen.todorovic@burda.com>
2019-09-12 17:57:14 +0200

    Adjusted review comments

--- test/travis-ci-run.sh
+++ test/travis-ci-run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Build Docker image for testing
 
@@ -14,7 +14,7 @@ WAIT_UNTIL_TIMESTAMP=$((CURRENT_TIMESTAMP + MAX_WAIT))
 
 # Wait for server to be ready
 until $(curl --output /dev/null --silent --head --fail "http://localhost:8080/"); do
-    if [ $(date +%s) -gt ${WAIT_UNTIL_TIMESTAMP} ]; then
+    if [[ $(date +%s) -gt "${WAIT_UNTIL_TIMESTAMP}" ]]; then
       echo "Max execution time exceeded"
 
       exit 1
@@ -34,5 +34,5 @@ docker exec "${TEST_THUNDER_PHP_DOCKER_ID}" su - thunder -c 'cd www; drush pml -
 docker exec "${TEST_THUNDER_PHP_DOCKER_ID}" su - thunder -c 'cd /home/thunder/www/docroot/core/node_modules/elastic-apm-node;'
 
 # Check that envirovment variables are set for thunder user
-docker exec "${TEST_THUNDER_PHP_DOCKER_ID}" su - thunder -c '[ "${THUNDER_HOST}" == "localhost" ] && [ ${CHROME_HOST} == "chrome" ] && echo "All Good!" || exit 1'
-docker exec "${TEST_THUNDER_PHP_DOCKER_ID}" su - thunder -c '[ ${ELASTIC_APM_URL} == "http://127.0.0.1:8200" ] && [ ${ELASTIC_APM_CONTEXT_TAG_BRANCH} == "travis-ci-test" ] && echo "All Good!" || exit 1'
+docker exec "${TEST_THUNDER_PHP_DOCKER_ID}" su - thunder -c '[ "${THUNDER_HOST}" == "localhost" ] && [ "${CHROME_HOST}" == "chrome" ] && echo "All Good!" || exit 1'
+docker exec "${TEST_THUNDER_PHP_DOCKER_ID}" su - thunder -c '[ "${ELASTIC_APM_URL}" == "http://127.0.0.1:8200" ] && [ "${ELASTIC_APM_CONTEXT_TAG_BRANCH}" == "travis-ci-test" ] && echo "All Good!" || exit 1'

964308cf48a07056f5a02ba90b1bbd1f88d70d95
Mladen Todorovic <mladen.todorovic@burda.com>
2019-09-19 14:16:38 +0200

    Fix path to Drupal console and adjust CI (#5)
    * Fix path for the drupal console
    
    * Adjusted test docker-compose to support file import
    
    * Adjusted Travis CI testing setup
    
    * Fixed issue with drush pm:list command
    
    * Adjusted changes from review comment r326125767

--- test/travis-ci-run.sh
+++ test/travis-ci-run.sh
@@ -28,7 +28,11 @@ done
 curl --silent "http://localhost:8080/" | grep --silent "meta.*Generator.*Thunder"
 
 # Check that relevant modules are installed
-docker exec "${TEST_THUNDER_PHP_DOCKER_ID}" su - thunder --command='cd www; drush pm:list --status=enabled --format=json;' | jq --exit-status '.thunder_performance_measurement, .testsite_builder, .media_entity_generic'
+docker exec "${TEST_THUNDER_PHP_DOCKER_ID}" su - thunder --command='cd /home/thunder/www/docroot; drush pm:list --status=enabled --format=json;' | jq --exit-status '.thunder_performance_measurement, .media_entity_generic'
+
+# Workaround for testsite_builder module name until Drush is fixed
+# TODO: Move check back to "drush pm:list" command when Drush issue is fixed (https://github.com/drush-ops/drush/issues/4182)
+docker exec "${TEST_THUNDER_PHP_DOCKER_ID}" su - thunder --command='cd /home/thunder/www/docroot; /home/thunder/www/vendor/drupal/console/bin/drupal debug:module --status=enabled;' | grep --silent "testsite_builder"
 
 # Check that required Node.JS package is installed for Elastic APM
 docker exec "${TEST_THUNDER_PHP_DOCKER_ID}" su - thunder --command='cd /home/thunder/www/docroot/core/node_modules/elastic-apm-node;'
@@ -37,6 +41,6 @@ docker exec "${TEST_THUNDER_PHP_DOCKER_ID}" su - thunder --command='cd /home/thu
 docker exec "${TEST_THUNDER_PHP_DOCKER_ID}" su - thunder --command='ls /home/thunder/www/docroot/thunder_test_site_template.json;'
 
 # Check that envirovment variables are set for thunder user
-docker exec "${TEST_THUNDER_PHP_DOCKER_ID}" su - thunder --command='[ "${THUNDER_HOST}" == "localhost" ] && [ "${CHROME_HOST}" == "chrome" ] && echo "All Good!" || exit 1'
+docker exec "${TEST_THUNDER_PHP_DOCKER_ID}" su - thunder --command='[ "${THUNDER_HOST}" == "thunder-php" ] && [ "${CHROME_HOST}" == "chrome" ] && echo "All Good!" || exit 1'
 docker exec "${TEST_THUNDER_PHP_DOCKER_ID}" su - thunder --command='[ "${ELASTIC_APM_URL}" == "http://127.0.0.1:8200" ] && [ "${ELASTIC_APM_CONTEXT_TAG_BRANCH}" == "travis-ci-test" ] && echo "All Good!" || exit 1'
 docker exec "${TEST_THUNDER_PHP_DOCKER_ID}" su - thunder --command='[ "${THUNDER_TEST_SITE_TEMPLATE}" == "https://raw.githubusercontent.com/thunder/thunder-performance-site-templates/master/thunder_base_set.json" ] && echo "All Good!" || exit 1'

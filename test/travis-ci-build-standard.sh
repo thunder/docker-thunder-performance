d832e8f55b897e8ec9bff55b4f0a1b43155a519f
Alex Pott <alex.a.pott@googlemail.com>
2020-05-05 12:38:02 +0100

    Fix checkout

--- test/travis-ci-build-standard.sh
+++ test/travis-ci-build-standard.sh
@@ -4,7 +4,7 @@
 
 # Get the latest version of Drupal. Ideally this would be more fixed but it is
 # tricky.
-git clone --depth=1 git@git.drupal.org:project/drupal.git drupal
+git clone --depth=1 https://git.drupalcode.org/project/drupal.git drupal
 
 # Build image with tag
 sh -x ./build.sh --tag "travis-ci-test" --profile standard --test-group standard --project-path ./drupal

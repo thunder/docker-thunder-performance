20f21786326f6f2ef329a242135898f5881d29d9
Alex Pott <alex.a.pott@googlemail.com>
2020-05-19 14:27:26 +0100

    Less composer steps and more chance of failing properly

--- build.sh
+++ build.sh
@@ -76,7 +76,6 @@ if [ "${PROJECT_PATH}" != "" ]; then
   fi
   # Compose project to ensure dependencies are correct.
   cd "${SCRIPT_DIRECTORY}/www"
-  composer install
   composer require drush/drush thunder/thunder_performance_measurement thunder/testsite_builder drupal/media_entity_generic drupal/console
   cd "${SCRIPT_DIRECTORY}"
 else

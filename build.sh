a2664a8f9a938426fcf73a50728ee1c7cdf162fb
alexpott <alex.a.pott@gmail.com>
2020-06-22 16:47:39 +0100

    Drupal core testing and apache instead of PHP builtin server.
    * Make it possible to build from a checkout of core and determine docroot dynamically
    
    * Add support for web as docroot
    
    * Fix coding style issues.
    
    * Add a test for standard
    
    * Improve build so composition is done prior to building docker image
    
    * Use Apache instead of PHP builtin server
    
    * Uninstall automated_cron and update as they affect performance tests based on time
    
    * Increase composer's memory limit
    
    * Use burda/thunder-php:apache
    
    * Allow Drush 10
    
    * Move composition around again
    
    * build: remove media_entity_generic
    
    Co-authored-by: Bosen Daniel <daniel.bosen@burda.com>
    Co-authored-by: Christian Fritsch <chr.fritsch@gmx.net>

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+set -e
+
 #
 # Build thunder performance docker image
 
 TAG_NAME=""
 PROJECT_PATH=""
+PROFILE="thunder"
+THUNDER_TEST_GROUP="Thunder_Base_Set"
 
 # Process script options
 while [ -n "$1" ]; do
@@ -16,12 +20,24 @@ while [ -n "$1" ]; do
         --project-path)
             # Check if correct directory path is provided
             if [ ! -d "$2" ]; then
-                echo "Provided directory path is not correct."
+                echo "Provided project path is not a directory."
 
                 exit 1
             fi
 
             PROJECT_PATH="$2"
+
+            shift
+            ;;
+
+        --profile)
+            PROFILE="$2"
+
+            shift
+            ;;
+
+        --test-group)
+            THUNDER_TEST_GROUP="$2"
 
             shift
             ;;
@@ -34,13 +50,32 @@ done
 
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# Copy Thunder prject to Dockerfile context if project path is provided
-if [[ "${PROJECT_PATH}" != "" ]]; then
-    cp --dereference --recursive "${PROJECT_PATH}" "${SCRIPT_DIRECTORY}/www"
+# Copy Thunder project to Dockerfile context if project path is provided
+if [ "${PROJECT_PATH}" != "" ]; then
+  rm -rf "${SCRIPT_DIRECTORY}/www"
+  cp -R "${PROJECT_PATH}" "${SCRIPT_DIRECTORY}/www"
+  # Compose project to ensure dependencies are correct.
+  cd "${SCRIPT_DIRECTORY}/www"
+  composer require drush/drush thunder/thunder_performance_measurement thunder/testsite_builder
+  cd "${SCRIPT_DIRECTORY}"
+#else
+  # @todo Ensure a the minimum of build tools.
+  # cd "${SCRIPT_DIRECTORY}/www"
+  # composer require drush/drush drupal/console
+  # cd "${SCRIPT_DIRECTORY}"
 fi
 
 # CleanUp project
-find "${SCRIPT_DIRECTORY}/www" -type d -name ".git" -print0 | xargs -0 rm --recursive --force
+# Coder has uncommitted changes due to Drupal's cleaner removing tests.
+rm -rf "${SCRIPT_DIRECTORY}/www/vendor/drupal/coder"
+
+# Note: do not use -d on composer as it can end up reverting changes.
+cd "${SCRIPT_DIRECTORY}/www"
+composer install --no-dev
+cd "${SCRIPT_DIRECTORY}"
+
+# Remove all git info for smaller docker images.
+find "${SCRIPT_DIRECTORY}/www" -type d -name ".git" -print0 | xargs -0 rm -rf
 
 # Build docker image
-docker build "${SCRIPT_DIRECTORY}" --tag "${TAG_NAME}"
+docker build --build-arg PROFILE="${PROFILE}" --build-arg THUNDER_TEST_GROUP="${THUNDER_TEST_GROUP}" "${SCRIPT_DIRECTORY}" --tag "${TAG_NAME}"
